f74f31e572e340cc073f02ac2caf2850e9e72f67
Omar AbdulAziz <omarabdulaziz451@gmail.com>
2019-12-04 18:32:26 -0800

    fixed some python code

--- DB.py
+++ DB.py
@@ -1,75 +1,36 @@
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
 #!/usr/bin/python
 import psycopg2
 from config import config
- 
+
 def connect():
     """ Connect to the PostgreSQL database server """
     conn = None
     try:
         # read connection parameters
         params = config()
-      #  print('{1}'.format(**params)) 
         # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(**params)
-        print('Connected') 
+        print('Connected')
         # create a cursor
         cur = conn.cursor()
-        
-   # execute a statement
+        # execute a statement
         print('PostgreSQL database version:')
         cur.execute('SELECT version()')
- 
         # display the PostgreSQL database server version
         db_version = cur.fetchone()
         print(db_version)
-       
+        #sql commands:
+
        # close the communication with the PostgreSQL
+        conn.commit()
         cur.close()
+        conn.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
         if conn is not None:
             conn.close()
             print('Database connection closed.')
- 
- 
 if __name__ == '__main__':
     connect()
